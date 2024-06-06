65e90fa439d687dd811be70233c5aadf11c96350
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2024-06-05 23:46:49 -0300

    feat: add the jakarta font to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className='flex w-screen h-screen bg-black'>
-      <h1 className=' text-white'>Hello world</h1>
+      <h1 className='text-evergreen-200'>Hello world</h1>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'evergreen-100': '#2B4341',
+        'evergreen-200': '#2AA78D',
+      }
+    },
   },
   plugins: [],
 }
